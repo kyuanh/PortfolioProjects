14e1528a3f9a620c7e38cea480bca61a39b07256
kyuanh <164593845+kyuanh@users.noreply.github.com>
2025-08-31 22:27:10 -0500

    Update Data Cleaning Layoff Project.sql

--- Data Cleaning Layoff Project.sql
+++ Data Cleaning Layoff Project.sql
@@ -1,51 +1,64 @@
+-- Viewing records from layoffs staging table
 SELECT *
 FROM layoffs_staging2;
 
+-- Finding the maximum number of employees laid off and the highest percentage laid off across all companies
 SELECT MAX(total_laid_off), MAX(percentage_laid_off)
 FROM layoffs_staging2;
 
+-- Finding companies that laid off 100% of their workforce
+-- Ordered by the largest amount of funds raised
 SELECT *
 FROM layoffs_staging2
 WHERE percentage_laid_off = 1
 ORDER BY funds_raised_millions DESC;
 
-SELECT company, SUM(total_laid_off)
+-- Show total layoffs per company, sorted by the the highest layoffs
+SELECT company, SUM(total_laid_off) 
 FROM layoffs_staging2
 GROUP BY company
 ORDER BY 2 DESC;
 
+-- Find the earliest and latest layoff dates
 SELECT MIN(`date`), MAX(`date`)
 FROM layoffs_staging2;
 
+-- Show total layoffs by country, sorted by highest layoffs
 SELECT country, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY country
 ORDER BY 2 DESC;
 
+-- Viewing records again for reference
 SELECT *
 FROM layoffs_staging2;
 
+-- Show total layoffs by year, sorted by year
 SELECT YEAR(`date`), SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY YEAR(`date`)
 ORDER BY 1 DESC;
 
+-- Show layoffs by company
 SELECT stage, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY stage
 ORDER BY 2 DESC;
 
+-- Show average percentage laid off per company, sorted by highest average
 SELECT company, AVG(percentage_laid_off)
 FROM layoffs_staging2
 GROUP BY company
 ORDER BY 2 DESC;
 
+-- Show monthly layoffs
 SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off)
 FROM layoffs_staging2
 WHERE SUBSTRING(`date`,1,7) IS NOT NULL 
 GROUP BY `MONTH`
 ORDER BY 1 ASC;
 
+-- Created a rolling total of layoffs over time
 WITH Rolling_Total AS 
 (
 SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off) AS total_off
@@ -58,17 +71,19 @@ SELECT `MONTH`, total_off,
 SUM(total_off) OVER(ORDER BY `MONTH`) AS rolling_total
 FROM Rolling_Total;
 
-
+-- Show total layoffs by company
 SELECT company, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY company
 ORDER BY 2 DESC;
 
+-- Show total layoffs per company per year
 SELECT company, YEAR(`date`), SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY company,YEAR (`date`)
 ORDER BY 3 DESC;
 
+-- Finding the top 5 companies with the most layoffs each year
 WITH Company_Year (company, years, total_laid_off) AS
 (
 SELECT company, YEAR(`date`), SUM(total_laid_off)
